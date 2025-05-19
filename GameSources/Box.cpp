00f795d0847fdda22df80c9e2df084969d8283ab
Hikaru Katono <zeal24350106@fsgcl.jp>
2025-05-19 16:13:01 +0900

    確認のためのBoxスティック移動コード

--- GameSources/Box.cpp
+++ GameSources/Box.cpp
@@ -9,6 +9,7 @@
 
 namespace basecross
 {
+
 	void Box::OnCreate()
 	{
 		//ドローコンポーネントの追加と設定
@@ -27,9 +28,99 @@ namespace basecross
 
 	}
 
+
 	void Box::OnUpdate()
 	{
+		auto G = GetStage()->GetThis<GameStage>()->GetGameObjectVec();
+		//コントローラチェックして入力があればコマンド呼び出し
+		MoveXZ();
 
 	}
+
+	Vec2 Box::GetInputState() const {
+		Vec2 ret;
+		//コントローラの取得
+		auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+		ret.x = 0.0f;
+		ret.y = 0.0f;
+		WORD wButtons = 0;
+
+		// 左スティックの状態を判定
+		if (cntlVec[0].bConnected) {
+			ret.x = cntlVec[0].fThumbLX;
+			ret.y = cntlVec[0].fThumbLY;
+		}
+		return ret;
+	}
+
+	Vec3 Box::GetMoveVector() const
+	{
+		Vec3 angle(0, 0, 0);
+		//入力の取得
+		auto inPut = GetInputState();
+		float moveX = inPut.x;
+		float moveZ = inPut.y;
+
+		if (moveX != 0 || moveZ != 0) 
+		{
+			auto ptrTransform = GetComponent<Transform>();
+			auto ptrCamera = OnGetDrawCamera();
+
+			// プレイヤーの回転角を考慮
+			//float playerRotationY = XMConvertToRadians(m_Rotation.y);
+
+			// カメラ方向からの角度
+			auto front = ptrTransform->GetPosition() - ptrCamera->GetEye();
+			front.y = 0;
+			front.normalize();
+			float frontAngle = -atan2(front.z, front.x);
+
+			// スティック入力の角度
+			Vec2 moveVec(moveX, moveZ);
+			float cntlAngle = atan2(-moveX, moveZ);
+
+			// 合計角度計算（カメラ + スティック + プレイヤー回転）
+			float totalAngle = frontAngle + cntlAngle;
+
+			// 角度から移動ベクトルを作成
+			angle = Vec3(cos(totalAngle), 0, sin(totalAngle));
+			angle.normalize();
+
+			// 移動サイズの適用
+			angle *= moveVec.length();
+
+			//x軸を固定
+			angle.x = 0;
+
+
+		}
+		return angle;
+	}
+
+	void Box::MoveXZ() {
+		auto angle = GetInputState();
+		float elapsedTime = App::GetApp()->GetElapsedTime();
+		auto pos = GetComponent<Transform>()->GetPosition();
+		pos += elapsedTime * m_velocity;
+		GetComponent<Transform>()->SetPosition(pos);
+	}
+
+	void Box::BoxMove()
+	{
+		float elapsedTime = App::GetApp()->GetElapsedTime();
+		auto angle = GetMoveVector();
+
+		if (angle.length() > 0.0f)
+		{
+			auto pos = GetComponent<Transform>()->GetPosition();
+			pos += angle * elapsedTime * 6.0f;
+			GetComponent<Transform>()->SetPosition(pos); // 更新後
+
+		}
+
+
+	}
+
+
 }
 //end basecross
--- GameSources/Box.h
+++ GameSources/Box.h
@@ -10,13 +10,35 @@
 namespace basecross {
 	class Box : public GameObject
 	{
+
+		Vec2 GetInputState() const;
+
+		Vec3 GetMoveVector() const;
+
+		float m_Speed;
+
+		bool m_isAir;
+
+		const float m_gravity;
+
+		Vec3 m_forward; // 前方向を示すベクトル
+		Vec3 m_velocity;
+
+		void MoveXZ();
+		void BoxMove();
+
 	public:
 		//コンストラクタ
 		Box(const shared_ptr<Stage>& stage) :
-			GameObject(stage)
+			GameObject(stage),
+			m_Speed(5.0f),
+			m_isAir(false),
+			m_gravity(-9.0)
 
 		{
 		}
+
+		virtual ~Box(){}
 
 		virtual void OnCreate() override;
 		virtual void OnUpdate() override;
