6318d46020a624b4774e1a6fdc48d7afb0991b44
Seino Mashiro <zeal24450159@fsgcl.jp>
2025-06-25 16:21:39 +0900

    編集

--- GameSources/Enemy.cpp
+++ GameSources/Enemy.cpp
@@ -114,7 +114,7 @@ namespace basecross {
 					m_hasMeowed = true;
 
 					auto ptrXA = App::GetApp()->GetXAudio2Manager();
-					m_CatSound = ptrXA->Start(L"CatVoice", 0, volumeSE);
+					m_CatSound = ptrXA->Start(L"CatVoice", 0, 1.0f);
 				}
 			}
 			// 距離がサウンドを鳴らす範囲外に出た場合
