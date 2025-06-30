67b4b808dc00b16961bdeb3d32dd9c468bf36209
Hikaru Katono <zeal24350106@fsgcl.jp>
2025-06-30 11:57:44 +0900

    GameStage2のエラー直し

--- GameSources/Box.cpp
+++ GameSources/Box.cpp
@@ -221,12 +221,15 @@ namespace basecross
 			Vec3 nextPos = currentPos + deltaMove; //Boxのポジションに移動の値を足す
 
 			// --- テーブルの範囲情報を取得 ---
-			auto stage = GetStage()->GetThis<GameStage>();
-			//auto stage2 = GetStage()->GetThis<GameStage2>();
+			auto stage = GetStage();
+			auto stage2 = GetStage();
 
-			if (stage) 
+			auto stageptr = dynamic_pointer_cast<GameStage>(stage);
+			auto stageptr2 = dynamic_pointer_cast<GameStage2>(stage);
+
+			if (stageptr) 
 			{
-				auto table = stage->GetTableObject(); // GameStageからTableオブジェクトを取得
+				auto table = stageptr->GetTableObject(); // GameStageからTableオブジェクトを取得
 				if (table) 
 				{
 					auto tableTransform = table->GetComponent<Transform>();
@@ -272,53 +275,53 @@ namespace basecross
 				currentTransform->SetPosition(nextPos);
 			}
 
-			//if (stage2)
-			//{
-			//	auto table2 = stage2->GetTableObject(); // GameStageからTableオブジェクトを取得
-			//	if (table2)
-			//	{
-			//		auto tableTransform = table2->GetComponent<Transform>();
-			//		Vec3 tablePos = tableTransform->GetPosition();   // テーブルの位置
-			//		Vec3 tableScale = tableTransform->GetScale();    // テーブルのスケール
+			if (stageptr2)
+			{
+				auto table = stageptr2->GetTableObject(); // GameStageからTableオブジェクトを取得
+				if (table)
+				{
+					auto tableTransform = table->GetComponent<Transform>();
+					Vec3 tablePos = tableTransform->GetPosition();   // テーブルの位置
+					Vec3 tableScale = tableTransform->GetScale();    // テーブルのスケール
 
-			//		// スケール補正係数 (例: 見た目が10倍なら10.0f)
-			//		float scaleCorrectionFactor = 10.0f;
-			//		Vec3 actualTableScale = tableScale * scaleCorrectionFactor; //テーブルのスケールが小さいため、スケールに値をかけて大きく設定する
+					// スケール補正係数 (例: 見た目が10倍なら10.0f)
+					float scaleCorrectionFactor = 10.0f;
+					Vec3 actualTableScale = tableScale * scaleCorrectionFactor; //テーブルのスケールが小さいため、スケールに値をかけて大きく設定する
 
-			//		// テーブルのX方向の範囲を計算
-			//		float tableMinX = tablePos.x - actualTableScale.x / 2.0f; //テーブルの左端限界
-			//		float tableMaxX = tablePos.x + actualTableScale.x / 2.0f; //テーブルの右端限界
-			//		// テーブルのZ方向の範囲を計算
-			//		float tableMinZ = tablePos.z - (actualTableScale.z - 4.0f) / 2.0f; //テーブルの手前限界
-			//		float tableMaxZ = tablePos.z + (actualTableScale.z + 4.0f) / 2.0f; //テーブルの奥行限界
+					// テーブルのX方向の範囲を計算
+					float tableMinX = tablePos.x - actualTableScale.x / 2.0f; //テーブルの左端限界
+					float tableMaxX = tablePos.x + actualTableScale.x / 2.0f; //テーブルの右端限界
+					// テーブルのZ方向の範囲を計算
+					float tableMinZ = tablePos.z - (actualTableScale.z - 4.0f) / 2.0f; //テーブルの手前限界
+					float tableMaxZ = tablePos.z + (actualTableScale.z + 4.0f) / 2.0f; //テーブルの奥行限界
 
-			//		// --- Boxの新しいX座標をテーブルの範囲内に制限 ---
-			//		// Boxの左端がテーブルの左端より内側の場合
-			//		if (nextPos.x - boxScaleHalved.x < tableMinX)
-			//		{
-			//			nextPos.x = tableMinX + boxScaleHalved.x; //Boxの位置をテーブルの左端限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
-			//		}
-			//		// Boxの右端がテーブルの右端より内側の場合
-			//		else if (nextPos.x + boxScaleHalved.x > tableMaxX)
-			//		{
-			//			nextPos.x = tableMaxX - boxScaleHalved.x; //Boxの位置をテーブルの右端限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
+					// --- Boxの新しいX座標をテーブルの範囲内に制限 ---
+					// Boxの左端がテーブルの左端より内側の場合
+					if (nextPos.x - boxScaleHalved.x < tableMinX)
+					{
+						nextPos.x = tableMinX + boxScaleHalved.x; //Boxの位置をテーブルの左端限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
+					}
+					// Boxの右端がテーブルの右端より内側の場合
+					else if (nextPos.x + boxScaleHalved.x > tableMaxX)
+					{
+						nextPos.x = tableMaxX - boxScaleHalved.x; //Boxの位置をテーブルの右端限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
 
-			//		}
-			//		// --- Boxの新しいZ座標をテーブルの範囲内に制限 ---
-			//		// Boxの手前側がテーブルの手前側より内側の場合
-			//		if (nextPos.z - boxScaleHalved.z < tableMinZ)
-			//		{
-			//			nextPos.z = tableMinZ + boxScaleHalved.z; //Boxの位置をテーブルの手前限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
-			//		}
-			//		// Boxの奥側がテーブルの奥側より内側の場合
-			//		else if (nextPos.z + boxScaleHalved.z > tableMaxZ)
-			//		{
-			//			nextPos.z = tableMaxZ - boxScaleHalved.z; //Boxの位置をテーブルの奥行限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
-			//		}
-			//	}
-			//	// --- 制限された新しい位置を適用 ---
-			//	currentTransform->SetPosition(nextPos);
-			//}
+					}
+					// --- Boxの新しいZ座標をテーブルの範囲内に制限 ---
+					// Boxの手前側がテーブルの手前側より内側の場合
+					if (nextPos.z - boxScaleHalved.z < tableMinZ)
+					{
+						nextPos.z = tableMinZ + boxScaleHalved.z; //Boxの位置をテーブルの手前限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
+					}
+					// Boxの奥側がテーブルの奥側より内側の場合
+					else if (nextPos.z + boxScaleHalved.z > tableMaxZ)
+					{
+						nextPos.z = tableMaxZ - boxScaleHalved.z; //Boxの位置をテーブルの奥行限界にBoxのサイズの半分の値を足した値の場所にする(押し出し)
+					}
+				}
+				// --- 制限された新しい位置を適用 ---
+				currentTransform->SetPosition(nextPos);
+			}
 
 		}
 	}
