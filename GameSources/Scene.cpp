b3b31a0a322a21365c72620fef2d145817189ea0
Hikaru Katono <zeal24350106@fsgcl.jp>
2025-07-08 14:09:02 +0900

    サウンド登録

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -44,6 +44,7 @@ namespace basecross{
 			app->RegisterWav(L"Fall2_SE", soundPath + L"Fall_SE.wav");
 			app->RegisterWav(L"CatVoice", soundPath + L"CatVoice.wav");
 			app->RegisterWav(L"Impact", soundPath + L"Impact3.wav");
+			app->RegisterWav(L"Tikutaku", soundPath + L"tikutaku.wav");
 
 
 
